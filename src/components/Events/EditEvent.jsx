4d373a20021e3843dac0945bced1287148341133
DiegoMayorga <diegoamayorgat@gmail.com>
2025-02-24 09:29:00 -0500

    Editing an event.

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,25 +1,30 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import { fetchEvent } from "../util/http.js";
+import { fetchEvent, updateEvent } from "../util/http.js";
 
 import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
+  const navigate = useNavigate();
   const params = useParams();
 
   const { data, isPending, isError, error } = useQuery({
-    // params.id porque esta consulta depende del id del evento que estamos tratando de editar.
     queryKey: ["events", params.id],
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id }),
   });
 
-  const navigate = useNavigate();
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+  });
 
-  function handleSubmit(formData) {}
+  function handleSubmit(formData) {
+    mutate({ id: params.id, event: formData });
+    navigate("../");
+  }
 
   function handleClose() {
     navigate("../");
